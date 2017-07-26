450298523a9b91e24e8fc7cb15296b7891e17161
Overrideveloper <bansowisdom@gmail.com>
2017-07-27 00:56:43 +0100

    Initial Commit

---
+++ SAAS-AIMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAAS_AIMS.MvcApplication" Language="C#" %>
---
+++ SAAS-AIMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAAS_AIMS.Startup))]
+namespace SAAS_AIMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
