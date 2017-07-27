378dc3fcf3bda918a5806299ed889f834d9fba24
Overrideveloper <bansowisdom@gmail.com>
2017-07-27 14:56:32 +0100

    Modified Account View Models

--- SAAS-AIMS/Models/AccountViewModels.cs
+++ SAAS-AIMS/Models/AccountViewModels.cs
@@ -65,6 +65,11 @@ namespace SAAS_AIMS.Models
         public string UserName { get; set; }
 
         [Required]
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
