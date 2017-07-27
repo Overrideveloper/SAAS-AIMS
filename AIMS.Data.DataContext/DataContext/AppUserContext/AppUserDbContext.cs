b7dc05eb97dd34b88bc28b5bb49e88f878667c07
Overrideveloper <bansowisdom@gmail.com>
2017-07-27 13:42:01 +0100

    Created data context and object class libraries

---
+++ AIMS.Data.DataContext/DataContext/AppUserContext/AppUserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMS.Data.DataContext.DataContext.AppUserContext
+{
+    public class AppUserContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
