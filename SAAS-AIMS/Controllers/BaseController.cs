069c222ca0428962e0ddc3381c0549459a12416d
Overrideveloper <bansowisdom@gmail.com>
2017-08-04 23:11:45 +0100

    Base and Dashboard controllers

---
+++ SAAS-AIMS/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAAS_AIMS.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                filterContext.HttpContext.Response.Redirect("/Account/Login");
+            }
+
+        }
+	}
+}
