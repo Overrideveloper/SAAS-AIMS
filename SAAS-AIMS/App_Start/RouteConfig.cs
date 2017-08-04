54da19016fcccc4a8435371bd3772bf7ad01b07c
Overrideveloper <bansowisdom@gmail.com>
2017-08-04 23:40:51 +0100

    Added Academic Session Statistics module

--- SAAS-AIMS/App_Start/RouteConfig.cs
+++ SAAS-AIMS/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace SAAS_AIMS
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ SAAS-AIMS/Controllers/SessionDetailsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAAS_AIMS.Controllers
+{
+    public class SessionDetailsController : Controller
+    {
+        //
+        // GET: /SessionDetails/
+        public ActionResult Statistics(long sessionid)
+        {
+            Session["sessionid"] = sessionid;
+            return View("Statistics");
+        }
+	}
+}
