a8f1911f81d539631ac2d80ff88085ddb53ef493
Overrideveloper <bansowisdom@gmail.com>
2017-08-05 17:52:07 +0100

    Added Association Member index view

---
+++ SAAS-AIMS/Controllers/MemberController.cs
@@ -0,0 +1,29 @@
+﻿using AIMS.Data.DataContext.DataContext.MemberDataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAAS_AIMS.Controllers
+{
+    public class MemberController : Controller
+    {
+        
+        private MemberDataContext _memberDataContext;
+        
+        public MemberController()
+        {
+            _memberDataContext = new MemberDataContext();
+        }
+
+        //
+        // GET: /Member/
+        public ActionResult Index()
+        {
+            var member = from m in _memberDataContext.Members
+                             select m;
+            return View(member);
+        }
+	}
+}
