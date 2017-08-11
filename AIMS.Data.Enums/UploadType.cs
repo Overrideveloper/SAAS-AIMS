963125f35b32003f7eed973f63b4e0b8dac0b1c3
Overrideveloper <bansowisdom@gmail.com>
2017-08-11 10:40:07 +0100

    Added UploadType enum

---
+++ AIMS.Data.Enums/UploadType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMS.Data.Enums
+{
+    public enum UploadType
+    {
+        Minutes,
+        Images,
+        Finances
+    }
+}
