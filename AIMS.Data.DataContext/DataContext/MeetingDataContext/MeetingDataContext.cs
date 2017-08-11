32e5dde10ef3eaf16011a1b828dbd923647cd914
Overrideveloper <bansowisdom@gmail.com>
2017-08-11 10:12:24 +0100

    Added Meeting data context

---
+++ AIMS.Data.DataContext/DataContext/MeetingDataContext/MeetingDataContext.cs
@@ -0,0 +1,26 @@
+﻿using AIMS.Data.DataObjects.Entities.Meeting;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMS.Data.DataContext.DataContext.MeetingDataContext
+{
+    public class MeetingDataContext : DbContext
+    {
+        public MeetingDataContext() :
+            base("Aims")
+        {
+        }
+
+        public DbSet<Meeting> Meetings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
