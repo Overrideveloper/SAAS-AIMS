076d101dde8ebfaf7480d9e029b58c69d653e39d
Overrideveloper <bansowisdom@gmail.com>
2017-08-14 23:12:19 +0100

    Added semester enum

---
+++ AIMS.Data.Enums/Enums.Semester/Semester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMS.Data.Enums.Enums.Semester
+{
+    public enum Semester
+    {
+        [Description("First Semester")]
+        [DisplayName("First Semester")]
+        First,
+
+        [Description("Second Semester")]
+        [DisplayName("Second Semester")]
+        Second
+    }
+}
