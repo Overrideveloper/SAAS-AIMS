69945f3970522d4031d3a64b4c4fe10a8181b85e
Overrideveloper <bansowisdom@gmail.com>
2017-08-30 23:41:30 +0100

    Added Expense controllers

---
+++ SAAS-AIMS/Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,147 @@
+﻿using AIMS.Data.DataContext.DataContext.ExpenseDataContext;
+using AIMS.Data.DataContext.DataContext.IncomeDataContext;
+using AIMS.Data.DataContext.DataContext.SessionDataContext;
+using AIMS.Data.DataObjects.Entities.Expense;
+using AIMS.Data.Enums.Enums.NotificationType;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAAS_AIMS.Controllers
+{
+    public class ExpenseCategoryController : BaseController
+    {
+        private readonly ExpenseDataContext _expenseDataContext;
+        private readonly SessionDataContext _sessionDataContext;
+        string sessionname = string.Empty;
+
+        #region constructor
+        public ExpenseCategoryController()
+        {
+            _expenseDataContext = new ExpenseDataContext();
+            _sessionDataContext = new SessionDataContext();
+        }
+        #endregion
+
+        #region get session name
+        public string GetSessionName()
+        {
+            var sess = _sessionDataContext.Sessions.Find(Convert.ToInt64(Session["sessionid"]));
+            sessionname = sess.Title;
+            return sessionname;
+        }
+        #endregion
+
+        #region academic session's expense category list
+        //
+        // GET: /ExpenseCategory/
+        [HttpGet]
+        [Authorize]
+        public ActionResult Index(long sessionid)
+        {
+            Session["sessionid"] = sessionid;
+            var category = _expenseDataContext.ExpenseCategory.Where(s => s.SessionID == sessionid).ToList();
+            var sess = _sessionDataContext.Sessions.Find(sessionid);
+            TempData["SessTitle"] = sess.Title;
+            return View(category.OrderBy(s => s.Title));
+        }
+        #endregion
+
+        #region create expense category
+        //
+        // GET: /ExpenseCategory/Create
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create()
+        {
+            var category = new ExpenseCategory();
+            return PartialView("Create", category);
+        }
+
+        //
+        // POST: /ExpenseCategory/Create
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ExpenseCategory category)
+        {
+            if (ModelState.IsValid)
+            {
+                var categoryVar = new ExpenseCategory
+                {
+                    Title = category.Title,
+                    SessionID = Convert.ToInt64(Session["sessionid"]),
+                    
+                    CreatedBy = Convert.ToInt64(Session["UserID"]),
+                    DateCreated = DateTime.Now,
+                    DateLastModified = DateTime.Now,
+                    LastModifiedBy = Convert.ToInt64(Session["UserID"])
+                };
+
+                _expenseDataContext.ExpenseCategory.Add(categoryVar);
+                _expenseDataContext.SaveChanges();
+                TempData["Success"] = "Expense category successfully created for " + GetSessionName();
+                TempData["NotificationType"] = NotificationType.Create.ToString();
+                return Json(new { success = true });
+            }
+            return PartialView("Create", category);
+        }
+        #endregion
+
+        #region edit expense category
+        //
+        // GET: /ExpenseCategory/Edit/id
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult> Edit(long id)
+        {
+            var category = await _expenseDataContext.ExpenseCategory.FindAsync(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("Edit", category);
+        }
+
+        //
+        // POST: /ExpenseCategory/Edit/id
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ExpenseCategory category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.DateLastModified = DateTime.Now;
+                category.LastModifiedBy = Convert.ToInt64(Session["UserID"]);
+
+                _expenseDataContext.Entry(category).State = EntityState.Modified;
+                _expenseDataContext.SaveChanges();
+                TempData["Success"] = "Expense category successfully modified for " + GetSessionName();
+                TempData["NotificationType"] = NotificationType.Edit.ToString();
+                return Json(new { success = true });
+            }
+            return PartialView("Edit", category);
+        }
+        #endregion
+
+        #region delete income category
+        //
+        // DELETE: /IncomeCategory/Delete/id
+        [Authorize]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var category = await _expenseDataContext.ExpenseCategory.FindAsync(id);
+            _expenseDataContext.ExpenseCategory.Remove(category);
+            await _expenseDataContext.SaveChangesAsync();
+            TempData["Success"] = "Expense category successfully deleted for " + GetSessionName();
+            TempData["NotificationType"] = NotificationType.Delete.ToString();
+            return RedirectToAction("Index", new { sessionid = Convert.ToInt64(Session["sessionid"]) });
+        }
+        #endregion
+    }
+}
---
+++ SAAS-AIMS/Controllers/ExpenseItemController.cs
@@ -0,0 +1,145 @@
+﻿using AIMS.Data.DataContext.DataContext.ExpenseDataContext;
+using AIMS.Data.DataContext.DataContext.IncomeDataContext;
+using AIMS.Data.DataObjects.Entities.Expense;
+using AIMS.Data.DataObjects.Entities.Income;
+using AIMS.Data.Enums.Enums.NotificationType;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAAS_AIMS.Controllers
+{
+    public class ExpenseItemController : BaseController
+    {
+        private readonly ExpenseDataContext _expenseDataContext;
+        string category = string.Empty;
+
+        #region constructor
+        public ExpenseItemController()
+        {
+            _expenseDataContext = new ExpenseDataContext();
+        }
+        #endregion
+
+        #region get category name
+        public string GetCategoryName()
+        {
+            var sess = _expenseDataContext.ExpenseCategory.Find(Convert.ToInt64(Session["expenseid"]));
+            category = sess.Title;
+            return category;
+        }
+        #endregion
+
+        #region expense item list
+        //
+        // GET: /ExpenseItem/
+        [HttpGet]
+        [Authorize]
+        public ActionResult Index(long expenseid)
+        {
+            Session["expenseid"] = expenseid;
+            var item = _expenseDataContext.ExpenseItem.Where(s => s.ExpenseCategoryID == expenseid).ToList();
+            var category = _expenseDataContext.ExpenseCategory.Find(expenseid);
+            TempData["category"] = category.Title;
+            return View(item.OrderBy(s => s.Title));
+        }
+        #endregion
+
+        #region create expense item
+        //
+        // GET: /ExpenseItem/Create
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create()
+        {
+            var item = new ExpenseItem();
+            return PartialView("Create", item);
+        }
+
+        //
+        // POST: /ExpenseItem/Create
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ExpenseItem item)
+        {
+            if (ModelState.IsValid)
+            {
+                var itemVar = new ExpenseItem
+                {
+                    Title = item.Title,
+                    ExpenseCategoryID = Convert.ToInt64(Session["expenseid"]),
+                    Amount = item.Amount,
+                    CreatedBy = Convert.ToInt64(Session["UserID"]),
+                    DateCreated = DateTime.Now,
+                    DateLastModified = DateTime.Now,
+                    LastModifiedBy = Convert.ToInt64(Session["UserID"])
+                };
+
+                _expenseDataContext.ExpenseItem.Add(itemVar);
+                _expenseDataContext.SaveChanges();
+                TempData["Success"] = "Expense item successfully created for " + GetCategoryName();
+                TempData["NotificationType"] = NotificationType.Create.ToString();
+                return Json(new { success = true });
+            }
+            return PartialView("Create", item);
+        }
+        #endregion
+
+        #region edit expense item
+        //
+        // GET: /ExpenseItem/Edit/id
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult> Edit(long id)
+        {
+            var item = await _expenseDataContext.ExpenseItem.FindAsync(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("Edit", item);
+        }
+
+        //
+        // POST: /ExpenseItem/Edit/id
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ExpenseItem item)
+        {
+            if (ModelState.IsValid)
+            {
+                item.DateLastModified = DateTime.Now;
+                item.LastModifiedBy = Convert.ToInt64(Session["UserID"]);
+
+                _expenseDataContext.Entry(item).State = EntityState.Modified;
+                _expenseDataContext.SaveChanges();
+                TempData["Success"] = "Expense item successfully modified for " + GetCategoryName();
+                TempData["NotificationType"] = NotificationType.Edit.ToString();
+                return Json(new { success = true });
+            }
+            return PartialView("Edit", item);
+        }
+        #endregion
+
+        #region delete income item
+        //
+        // DELETE: /ExpenseItem/Delete/id
+        [Authorize]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var item = await _expenseDataContext.ExpenseItem.FindAsync(id);
+            _expenseDataContext.ExpenseItem.Remove(item);
+            await _expenseDataContext.SaveChangesAsync();
+            TempData["Success"] = "Expense category successfully deleted for " + GetCategoryName();
+            TempData["NotificationType"] = NotificationType.Delete.ToString();
+            return RedirectToAction("Index", new { categoryid = Convert.ToInt64(Session["expenseid"]) });
+        }
+        #endregion
+	}
+}
