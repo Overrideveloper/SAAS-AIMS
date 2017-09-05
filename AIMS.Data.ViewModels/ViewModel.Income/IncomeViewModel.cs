fde19938717558550d0da99afd46adfb71cc6cc2
Overrideveloper <bansowisdom@gmail.com>
2017-09-05 01:15:23 +0100

    Added view models

---
+++ AIMS.Data.ViewModels/ViewModel.Income/IncomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMS.Data.ViewModels.ViewModel.Income
+{
+    public class IncomeViewModel
+    {
+        public string category { get; set; }
+        public decimal amount { get; set; }
+    }
+}
